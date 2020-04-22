00854cb23a709de50e366e954f9417bddb4c4663
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-04-22 10:36:19 +0200

    Added CCMatrix class

---
+++ src/CCMatrix.py
@@ -0,0 +1,10 @@
+from numpy import genfromtxt
+import pandas as pd
+
+
+class CCMatrix:
+    def __init__(self, matrix_csv, ages_csv):
+        self.cc_matrix = pd.read_csv(matrix_csv).to_numpy()
+        self.age_array = genfromtxt(ages_csv, delimiter=',')[:85]
+        print('cc matrix with size', self.cc_matrix.size, 'inserted')
+        print('age array with size', self.age_array.size, 'inserted')
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from src.CCMatrix import CCMatrix
+
+
+def main():
+    CCMatrix('../data/cc15.csv', '../data/leeftijden.csv')
+
+
+if __name__ == "__main__":
+    main()
