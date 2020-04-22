e95829dbc16eb0c9717a254b01aed64778e32b18
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-04-22 11:04:19 +0200

    Fixed cc matrix

--- src/CCMatrix.py
+++ src/CCMatrix.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 class CCMatrix:
     def __init__(self, matrix_csv, ages_csv):
-        self.cc_matrix = pd.read_csv(matrix_csv).to_numpy()
+        self.cc_matrix = pd.read_csv(matrix_csv, header=None, delimiter=';').to_numpy()
         self.age_array = genfromtxt(ages_csv, delimiter=',')[:85]
         print('cc matrix with size', self.cc_matrix.size, 'inserted')
         print('age array with size', self.age_array.size, 'inserted')
