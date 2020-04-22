ad6007ded0ca4d695a030d40903f1c853c8eb8b7
jdepreter <jolan.depreter@gmail.com>
2020-04-22 12:01:57 +0200

    infected

--- src/CCMatrix.py
+++ src/CCMatrix.py
@@ -5,6 +5,6 @@ import pandas as pd
 class CCMatrix:
     def __init__(self, matrix_csv, ages_csv):
         self.cc_matrix = pd.read_csv(matrix_csv, header=None, delimiter=';').to_numpy()
-        self.age_array = genfromtxt(ages_csv, delimiter=',')[:85]
+        self.age_array = genfromtxt(ages_csv, delimiter=',')[:86]
         print('cc matrix with size', self.cc_matrix.size, 'inserted')
         print('age array with size', self.age_array.size, 'inserted')
--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
 from src.CCMatrix import CCMatrix
+import numpy as np
+
+# Eerste infected persoon beinvloed wss de curve
+
+transmission_rate = 0.5
 
 
 def main():
-    CCMatrix('../data/cc15.csv', '../data/leeftijden.csv')
+    cc = CCMatrix('../data/cc15.csv', '../data/leeftijden.csv')
+    contact_matrix = cc.cc_matrix
+    susceptible = cc.age_array
+    exposed = np.zeros(86)
+    infected = np.zeros(86)
+    infected[22] = 1
+    infected[21] = 1
+    recovered = np.zeros(86)
+    contact_matrix = contact_matrix * transmission_rate
+
+    for i in range(5):
+        x = np.transpose(np.asmatrix(susceptible))
+        # susceptible_susceptible = np.matmul(x, np.asmatrix(susceptible))
+        susceptible_infected = np.matmul(x, np.asmatrix(infected))
+
+        contacts = np.multiply(contact_matrix, susceptible_infected)
+        contacts = np.asarray(contacts)
+        # print(contacts)
+
+        for column in contacts.transpose():
+            infected += column
+            susceptible -= column
+        susceptible = np.maximum(susceptible, np.zeros(86))
+
+    print(infected.sum())
 
 
 if __name__ == "__main__":
