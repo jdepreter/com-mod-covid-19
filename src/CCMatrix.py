03bdd536a2b3ab5116db9b31227e387f37e90f08
SamLegrand <sam.legrand@student.uantwerpen.be>
2020-04-23 17:24:25 +0200

    Fixed CCMatrix path on windows, updated some parameters

--- src/CCMatrix.py
+++ src/CCMatrix.py
@@ -1,9 +1,15 @@
 from numpy import genfromtxt
 import pandas as pd
+from os.path import dirname, abspath
+import os
+
 
 
 class CCMatrix:
     def __init__(self, matrix_csv, ages_csv):
+        data_folder = os.path.join(dirname(dirname(abspath(__file__))), 'data')
+        matrix_csv = os.path.join(data_folder, matrix_csv)
+        ages_csv = os.path.join(data_folder, ages_csv)
         self.cc_matrix = pd.read_csv(matrix_csv, header=None, delimiter=';').to_numpy()
         self.age_array = genfromtxt(ages_csv, delimiter=',')[:86]
         print('cc matrix with size', self.cc_matrix.size, 'inserted')
