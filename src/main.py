a8bce12083ea02d597334d54b3b6644bf31c20bc
Sam Legrand <sam.legrand@student.uantwerpen.be>
2020-04-27 16:48:29 +0200

    Moved model to separate class, implemented automatic determination of infectious_rate based on reference data

--- src/main.py
+++ src/main.py
@@ -1,58 +1,63 @@
 from src.CCMatrix import CCMatrix
+from src.Model import Model
+import matplotlib.pyplot as plt
 import numpy as np
 
 # Eerste infected persoon beinvloed wss de curve
 
-infectious_rate = 0.05
-# incubation_rate = 1.0/7.0   # incubation period of 7 days
-recovery_rate = 1.0/21.0    # infectious period of 21 days
-contact_reduction = 5       # contact reduction after 30 days (x times less contacts)
-
 
 def main():
+    infectious_rate = 0.05
+    incubation_rate = 1.0 / 3.0  # incubation period of 7 days
+    recovery_rate = 1.0 / 14.0  # infectious period of 21 days
     cc = CCMatrix('cc15.csv', 'leeftijden.csv')
-    contact_matrix = cc.cc_matrix
-    susceptible = cc.age_array
-    exposed = np.zeros(86)
-    infected = np.zeros(86)
-    infected[35] = 2
-    recovered = np.zeros(86)
-    contact_matrix = contact_matrix * infectious_rate
 
-    for i in range(60):
-        x = np.transpose(np.asmatrix(susceptible))
-        # susceptible_susceptible = np.matmul(x, np.asmatrix(susceptible))
-        if i == 30:
-            contact_matrix *= 1/contact_reduction
-        infected_new, susceptible = infect(contact_matrix, infected.copy(), susceptible, x)
-        recoveries = infected * recovery_rate
-        recovered += recoveries
-        infected_new -= recoveries
-        infected_new = np.maximum(infected_new, np.zeros(86))
+    reference_infected = np.fromfunction(lambda x, y: 2**(x/2.34), (60, 1), dtype=int)
 
-        infected = infected_new
-        print(susceptible.sum() + infected.sum() + recovered.sum())
-        print(i)
+    # initial model, do once with higher infectious rate, once with lower, continue with best fit
+    inf_rate_diff = 0.001
+    model = Model(cc, infectious_rate + inf_rate_diff, incubation_rate, recovery_rate)
+    model.run(60)
+    residual = np.absolute(model.infected_data - reference_infected)
+    pos_sum_of_squares = np.sum(residual**2)
 
-    print('---')
-    print(susceptible.sum())
-    print(infected.sum())
-    print(recovered.sum())
+    inf_rate_diff = -0.001
+    model = Model(cc, infectious_rate + inf_rate_diff, incubation_rate, recovery_rate)
+    model.run(60)
+    residual = np.absolute(model.infected_data - reference_infected)
+    neg_sum_of_squares = np.sum(residual ** 2)
 
-    print(susceptible.sum() + infected.sum() + recovered.sum())
+    if neg_sum_of_squares < pos_sum_of_squares:
+        infectious_rate -= 0.001
+        prev_sum_of_squares = neg_sum_of_squares
+        while True:
+            infectious_rate -= 0.001
+            print(infectious_rate)
+            model = Model(cc, infectious_rate, incubation_rate, recovery_rate)
+            model.run(60)
+            residual = np.absolute(model.infected_data - reference_infected)
+            sum_of_squares = np.sum(residual ** 2)
+            if sum_of_squares > prev_sum_of_squares or infectious_rate <= 0 or infectious_rate >= 1:
+                break
+    else:
+        infectious_rate += 0.001
+        prev_sum_of_squares = pos_sum_of_squares
+        while True:
+            infectious_rate += 0.001
+            print(infectious_rate)
+            model = Model(cc, infectious_rate, incubation_rate, recovery_rate)
+            model.run(60)
+            residual = np.absolute(model.infected_data - reference_infected)
+            sum_of_squares = np.sum(residual ** 2)
+            if sum_of_squares > prev_sum_of_squares or infectious_rate <= 0 or infectious_rate >= 1:
+                break
 
 
-def infect(contact_matrix, infected, susceptible, x):
-    susceptible_infected = np.matmul(x, np.asmatrix(infected))
-    contacts = np.multiply(contact_matrix, susceptible_infected)
-    contacts = np.asarray(contacts)
-
-    for column in contacts.transpose():
-        column = np.minimum(susceptible, column)
-        infected += column
-        susceptible -= column
-    susceptible = np.maximum(susceptible, np.zeros(86))
-    return infected, susceptible
+    plt.plot(model.infected_data, label='Model infected')
+    plt.plot(model.recovered_data, label='Model recovered')
+    plt.plot(reference_infected, label='Reference infected')
+    plt.legend()
+    plt.show()
 
 
 if __name__ == "__main__":
